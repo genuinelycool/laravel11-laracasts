9d742bedf302f412944d53c342588d11d9cdf4f9
genuinelycool <genuinelycool@gmail.com>
2025-02-14 12:10:56 +0545

    18. editing, updating, and deleting a resource

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// INDEX
 Route::get('/jobs', function() {
     // $jobs = Job::all();                  // lazy loading
     // $jobs = Job::with('employer')->get();   // eager loading
@@ -22,16 +23,19 @@ Route::get('/jobs', function() {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     // dd(request()->all());
     // dd(request('title'));
@@ -48,6 +52,57 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (On hold...)
+
+    // update the job
+    // $job = Job::find($id);
+    $job = Job::findOrFail($id);    // null
+    
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // and persist
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+
+    // delete the job
+    // $job = Job::findOrFail($id);
+    // $job->delete();
+    $job = Job::findOrFail($id)->delete();
+
+    // redirect
+    return redirect('/jobs');
+
 });
 
 Route::get('/contact', function () {
