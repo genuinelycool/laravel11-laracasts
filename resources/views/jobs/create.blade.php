8e26337f58edddd6bb22eba4d562a66340b0e3db
genuinelycool <genuinelycool@gmail.com>
2025-02-16 12:16:28 +0545

    19.1 remove all comments

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -38,16 +38,6 @@
                         </div>
                     </div>
                 </div>
-
-                <!-- <div class="mt-10">
-                    @if($errors->any())
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li class="text-red-500 italic">{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                </div> -->
             </div>
         </div>
 
