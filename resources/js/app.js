0d1905ff3a45d8b29c09579c0a97c836931a4476
genuinelycool <genuinelycool@gmail.com>
2025-02-19 21:49:45 +0545

    26. get you build process in order

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 import './bootstrap';
+import "../css/app.css";    // use this way for SPA (Single Page Application)
+
+// alert('hello from the JS');
--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>My Website</title>
+    
+    @vite(['resources/js/app.js'])
+
 </head>
 <body class="h-full">
 <div class="min-h-full">
